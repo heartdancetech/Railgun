4b52ad76577a5dc55446e299e9885d753eaddcfc
gsxhnd <gsxhnd@gmail.com>
2019-10-09 18:41:55 +0800

    test

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+// NewReverseProxy 创建反向代理处理方法
+func NewReverseProxy() *httputil.ReverseProxy {
+
+	var target = struct{
+		ServiceAddress string
+		ServicePort int
+	}{
+		ServiceAddress: "localhost",
+		ServicePort: 8081,
+	}
+	//创建Director
+	director := func(req *http.Request) {
+
+		//查询原始请求路径，如：/arithmetic/calculate/10/5
+		reqPath := req.URL.Path
+		if reqPath == "" {
+			return
+		}
+		//按照分隔符'/'对路径进行分解，获取服务名称serviceName
+		pathArray := strings.Split(reqPath, "/")
+		serviceName := pathArray[1]
+		fmt.Println(serviceName)
+
+		//重新组织请求路径，去掉服务名称部分
+		destPath := strings.Join(pathArray[2:], "/")
+
+		//随机选择一个服务实例
+		//tgt := result[rand.Int()%len(result)]
+		//logger.Log("service id", tgt.ServiceID)
+
+		//设置代理服务地址信息
+		req.URL.Scheme = "http"
+		req.URL.Host = fmt.Sprintf("%s:%d", target.ServiceAddress, target.ServicePort)
+		req.URL.Path = "/" + destPath
+	}
+
+	modifyResponse := func(res *http.Response) error {
+		fmt.Println(res.Body)
+		return nil
+	}
+
+	return &httputil.ReverseProxy{Director: director, ModifyResponse: modifyResponse}
+
+}
+
+
+func main() {
+	//创建反向代理
+	proxy := NewReverseProxy()
+
+	errc := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+		errc <- fmt.Errorf("%s", <-c)
+	}()
+
+	//开始监听
+	go func() {
+		errc <- http.ListenAndServe(":9090", proxy)
+	}()
+
+	<-errc
+}
