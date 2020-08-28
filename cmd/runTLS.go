0e93e24c5c2275597accb74640e0c8a6d6bc5a9d
gsxhnd <gsxhnd@gmail.com>
2020-08-28 13:55:48 +0800

    refactor: change enable-manage flag to dashboard

--- cmd/runTLS.go
+++ cmd/runTLS.go
@@ -2,44 +2,38 @@ package cmd
 
 import (
 	"bytes"
-	"errors"
 	"github.com/gsxhnd/owl"
 	"github.com/railgun-project/railgun/api"
 	"github.com/railgun-project/railgun/core"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/urfave/cli/v2"
 )
 
-func init() {
-	runTLSCmd.PersistentFlags().StringArrayVarP(&etcdUrlArry, "etcds", "e", []string{"127.0.0.1:2379"}, "")
-	runTLSCmd.PersistentFlags().BoolVar(&enableManage, "enable-manage", false, "")
-	runTLSCmd.PersistentFlags().StringVar(&certFile, "certFile", "", "")
-	runTLSCmd.PersistentFlags().StringVar(&keyFile, "keyFile", "", "")
-}
-
 var certFile, keyFile string
-var runTLSCmd = &cobra.Command{
-	Use:     "runTLS",
-	Short:   "run with tls",
-	Long:    "run with tls",
-	Example: "runTLS",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 || len(args) > 1 {
-			return errors.New("need key or too manay args")
-		}
-		return nil
+var runTLSCmd = &cli.Command{
+	Name:   "runTLS",
+	Usage:  "runTLS",
+	Before: nil,
+	After:  nil,
+	Flags: []cli.Flag{
+		&cli.StringSliceFlag{Name: "etcds", Aliases: []string{"e"}, Destination: etcdUrlArry},
+		&cli.BoolFlag{Name: "dashboard", Value: false, Destination: &enableManage},
+		&cli.StringFlag{Name: "certFile", Required: true, Destination: &certFile},
+		&cli.StringFlag{Name: "keyFile", Required: true, Destination: &keyFile},
 	},
-	PreRun: func(cmd *cobra.Command, args []string) {
-		owl.SetAddr(etcdUrlArry)
-		confKey := args[0]
+	Action: func(ctx *cli.Context) error {
+		owl.SetAddr(etcdUrlArry.Value())
+		confKey := ctx.Args().Get(0)
 		confStr, err := owl.GetByKey(confKey)
 		if err != nil {
+			return err
 		}
 		viper.SetConfigType("yaml")
 		err = viper.ReadConfig(bytes.NewBuffer([]byte(confStr)))
 		if err != nil {
-			panic(err)
+			return err
 		}
+
 		c := make(chan string)
 		go owl.Watcher(confKey, c)
 		go func() {
@@ -47,8 +41,7 @@ var runTLSCmd = &cobra.Command{
 				_ = viper.ReadConfig(bytes.NewBuffer([]byte(i)))
 			}
 		}()
-	},
-	Run: func(cmd *cobra.Command, args []string) {
+
 		if enableManage {
 			go api.RunTLS(certFile, keyFile)
 		}
@@ -56,5 +49,6 @@ var runTLSCmd = &cobra.Command{
 		core.SetMode(viper.GetString("run_mode"))
 		g := core.New()
 		_ = g.RunTLS(viper.GetString("addr"), certFile, keyFile)
+		return nil
 	},
 }
